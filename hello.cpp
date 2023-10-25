6f0539cd1b62cbf32f52810980ef7226f8d49b1a
alvaro-vinuela <alvaro@severalnines.com>
2023-10-25 10:01:45 +0200

    master branch evolves

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 #include "hello.h"
 
 void hello(){
-  std::cout << "Hello World This is master branch, and it is different from release!\n";
+  std::cout << "Hello World This is master branch, and it is different from release, it has its own evolution!\n";
 	std::cout << "Hotfix 1 on release!\n";
 }
