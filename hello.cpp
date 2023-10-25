3c282da81aaedc74cbb96a7629ca78f586aa4eaf
alvaro-vinuela <alvaro@severalnines.com>
2023-10-25 09:54:28 +0200

    master is different

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,5 @@
 #include "hello.h"
 
 void hello(){
-	std::cout << "Hello World This is release_1 branch!\n";
+	std::cout << "Hello World This is master branch, and it is different from release!\n";
 }
