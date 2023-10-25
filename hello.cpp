2efde6f7e59213838f7c3f153e70203adeae4cc5
alvaro-vinuela <alvaro@severalnines.com>
2023-10-25 10:04:59 +0200

    hotfix 1 on master is different

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,5 @@
 
 void hello(){
   std::cout << "Hello World This is master branch, and it is different from release, it has its own evolution!\n";
-	std::cout << "Hotfix 1 on release!\n";
+	std::cout << "Hotfix 1 on master is different!\n";
 }
